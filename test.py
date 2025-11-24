040ffa392dbf6bc7172cf40b51724ef37dca0409
Muhammad Shoaib Tahir <98810920+MuhammadshoaibTahir@users.noreply.github.com>
2025-11-24 13:59:42 +0500

    Update test.py

--- test.py
+++ test.py
@@ -28,7 +28,7 @@ from collections import Counter, defaultdict
 import streamlit as st
 st.set_page_config(page_title="Emotion Trajectory Studio", layout="wide", initial_sidebar_state="expanded")
 st.markdown(
-    "<h4 style='text-align:center; color:gray;'>Created by YOUR NAME</h4>",
+    "<h4 style='text-align:center; color:gray;'>Created by Muhammad Shoaib Tahir</h4>",
     unsafe_allow_html=True
 )
 
